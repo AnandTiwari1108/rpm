d5b717726019a8bfd237f9f71c02e20cda742c10
Shade1912 <pushkarsave1912@gmail.com>
2022-01-24 20:05:26 +0530

    html changes

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -15,12 +15,11 @@ function Signin() {
   });
   return (
     <div>
-      <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
       <div className="container" id="container">
         <div className="form-container sign-up-container">
           <form action="#">
-            <h1>Create Account</h1>
-            <div className="social-container">
+            <h1>Doctor Sign In</h1>
+            {/*<div className="social-container">
               <a href="#" className="social">
                 <i className="fab fa-facebook-f"></i>
               </a>
@@ -31,17 +30,17 @@ function Signin() {
                 <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <span>or use your account</span>*/}
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#">Forgot your password?</a>
+              <button>Sign In</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
           <form action="#">
-            <h1>Sign in</h1>
-            <div className="social-container">
+            <h1>Patient Sign In</h1>
+            {/*<div className="social-container">
               <a href="#" className="social">
                 <i className="fab fa-facebook-f"></i>
               </a>
@@ -52,7 +51,7 @@ function Signin() {
                 <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
-            <span>or use your account</span>
+            <span>or use your account</span>*/}
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
@@ -62,41 +61,24 @@ function Signin() {
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
+              <h1>Patient</h1>
               <p>
-                To keep connected with us please login with your personal info
+                For Patient Sign In
               </p>
               <button className="ghost" id="signIn">
-                Sign In
+                Click Here
               </button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
+              <h1>Doctor</h1>
+              <p>For Doctor Sign In</p>
               <button className="ghost" id="signUp">
-                Sign Up
+                Click Here
               </button>
             </div>
           </div>
         </div>
       </div>
-
-      <footer>
-        <p>
-          Created with <i className="fa fa-heart"></i> by
-          <a target="_blank" href="https://florin-pop.com">
-            Florin Pop
-          </a>
-          - Read how I created this and how you can join the challenge
-          <a
-            target="_blank"
-            href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/"
-          >
-            here
-          </a>
-          .
-        </p>
-      </footer>
     </div>
   );
 }
